3c8c1a857453dd11587a61276d8b0e2a0bb7de5a
Nyabsi <nyabsi@sovellus.cc>
2026-04-26 20:27:02 +0300

    fix: ensure we don't track devices that don't exist yet

--- src/Main.cpp
+++ src/Main.cpp
@@ -116,9 +116,11 @@ int main(
     UpdateApplicationRefreshRate();
 
     for (uint32_t i = 0; i < vr::k_unMaxTrackedDeviceCount; i++) {
-        if (i == vr::k_unTrackedDeviceIndex_Hmd)
-            continue;
-        g_processInformation->AddMonitoredDeviceById(i);
+        try {
+            auto properties = VrTrackedDeviceProperties::FromDeviceIndex(i);
+            properties.CheckConnection(); // ensure we don't fill slots for devices that are not yet connected.
+            g_processInformation->AddMonitoredDeviceById(i);
+        } catch (...) { /* ... */ }
     }
 
     vr::VREvent_t vr_event = {};
